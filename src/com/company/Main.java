11ab432638149f605f9611aadd291421547226d6
Mantas <mantas.muzzz@gmail.com>
2018-02-20 09:27:40 +0200

    padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        int skaicius;
+        Scanner in = new Scanner(System.in);
+        Main obj = new Main();
 
+        System.out.println("Iveskite Skaiciu");
+        skaicius = in.nextInt();
+
+        obj.arLyginis(skaicius);
+    }
+
+    public void arLyginis(int sk){
+        if (sk % 2 == 0){
+            System.out.println("skaicius lyginis");
+        }else {
+            System.out.println("Skaicius nelyginis");
+        }
     }
 }
